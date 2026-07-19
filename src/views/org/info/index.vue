8fff29a2d6e506c4d94e7ae6256b4dc4b0be0721
William.wei <William.wei@g-towm.com.cn>
2026-07-19 08:28:27 +0800

    feat: 机构信息默认只读，编辑按钮切换维护模式
    - 默认所有字段 disabled，右上角显示「编辑」按钮
    - 点击编辑后字段可写，右上角切换为「取消 / 保存」
    - 取消时恢复进入编辑前的数据（backup 快照）
    - 保存成功后重新加载数据并退出编辑模式

--- src/views/org/info/index.vue
+++ src/views/org/info/index.vue
@@ -1,6 +1,17 @@
 <template>
   <el-card v-loading="loading" shadow="never" class="page">
-    <template #header><span class="card-title">机构信息维护</span></template>
+    <template #header>
+      <div class="card-header">
+        <span class="card-title">机构信息</span>
+        <template v-if="editForm.id">
+          <template v-if="editing">
+            <el-button @click="cancelEdit">取消</el-button>
+            <el-button v-permission="['org:info:update']" type="primary" :loading="submitting" @click="handleSave">保存</el-button>
+          </template>
+          <el-button v-else v-permission="['org:info:update']" type="primary" @click="editing = true">编辑</el-button>
+        </template>
+      </div>
+    </template>
     <el-form
       v-if="editForm.id"
       :model="editForm"
@@ -9,18 +20,14 @@
     >
       <el-divider content-position="left">基本信息</el-divider>
       <el-form-item label="机构编码"><el-input v-model="editForm.code" disabled /></el-form-item>
-      <el-form-item label="机构名称"><el-input v-model="editForm.name" /></el-form-item>
-      <el-form-item label="备注"><el-input v-model="editForm.remark" type="textarea" :rows="2" /></el-form-item>
+      <el-form-item label="机构名称"><el-input v-model="editForm.name" :disabled="!editing" /></el-form-item>
+      <el-form-item label="备注"><el-input v-model="editForm.remark" type="textarea" :rows="2" :disabled="!editing" /></el-form-item>
 
       <el-divider content-position="left">联系方式</el-divider>
-      <el-form-item label="联系人"><el-input v-model="editForm.contact" /></el-form-item>
-      <el-form-item label="联系电话"><el-input v-model="editForm.phone" /></el-form-item>
-      <el-form-item label="邮箱"><el-input v-model="editForm.email" /></el-form-item>
-      <el-form-item label="地址"><el-input v-model="editForm.address" /></el-form-item>
-
-      <el-form-item>
-        <el-button v-permission="['org:info:update']" type="primary" :loading="submitting" @click="handleSave">保存</el-button>
-      </el-form-item>
+      <el-form-item label="联系人"><el-input v-model="editForm.contact" :disabled="!editing" /></el-form-item>
+      <el-form-item label="联系电话"><el-input v-model="editForm.phone" :disabled="!editing" /></el-form-item>
+      <el-form-item label="邮箱"><el-input v-model="editForm.email" :disabled="!editing" /></el-form-item>
+      <el-form-item label="地址"><el-input v-model="editForm.address" :disabled="!editing" /></el-form-item>
     </el-form>
     <el-empty v-else description="暂无机构信息" />
   </el-card>
@@ -33,7 +40,9 @@ import { listOrgs, updateOrg, type OrgInfoDTO } from '@/api/org/info'
 
 const loading = ref(false)
 const submitting = ref(false)
+const editing = ref(false)
 const editForm = reactive<OrgInfoDTO>({})
+const backup = ref<OrgInfoDTO>({})
 
 async function loadOrg() {
   loading.value = true
@@ -42,10 +51,16 @@ async function loadOrg() {
     const list = Array.isArray(res) ? res : (res.list || res.records || [])
     if (list.length > 0) {
       Object.assign(editForm, list[0])
+      backup.value = { ...list[0] }
     }
   } finally {
     loading.value = false
   }
+}
+
+function cancelEdit() {
+  Object.assign(editForm, backup.value)
+  editing.value = false
 }
 
 async function handleSave() {
@@ -54,7 +69,8 @@ async function handleSave() {
   try {
     await updateOrg(editForm.id, { ...editForm })
     ElMessage.success('保存成功')
-    loadOrg()
+    editing.value = false
+    await loadOrg()
   } finally {
     submitting.value = false
   }
@@ -65,5 +81,6 @@ onMounted(loadOrg)
 
 <style scoped>
 .page { height: 100%; }
+.card-header { display: flex; align-items: center; justify-content: space-between; }
 .card-title { font-weight: 600; }
 </style>
